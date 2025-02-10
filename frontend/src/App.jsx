29f536bdf91b9ac0c7382e32495070059c0d3427
Adhiraj <adhirajgu22@iitk.ac.in>
2025-02-10 13:18:41 +0530

    frontend is also ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import { Button } from '@chakra-ui/react'
+import { Box,useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
 
 const App = () => {
   return (
-    <>
-      <Navbar/>
-    </>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+			</Routes>
+		</Box>
   )
 }
 export default App
